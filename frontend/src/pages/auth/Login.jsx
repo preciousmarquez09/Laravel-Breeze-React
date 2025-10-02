99defd33f683ad0629a0a8b93daf87284d9bef28
Precious <precious.marquez@adish.com.ph>
2025-10-02 16:35:39 +0800

    feat: login ui, tailwind classes for tags

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,7 +1,79 @@
+import { useState } from "react";
+import { CiMail } from "react-icons/ci";
+import { CiLock } from "react-icons/ci";
+import { LuEyeClosed } from "react-icons/lu";
+import { LuEye } from "react-icons/lu";
+import { GiCoffeeBeans } from "react-icons/gi";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <div className="flex-center min-h-[calc(100vh-72px)] w-full ">
+      <div className="card bg-white border-t-5 border-mocha">
+        <div className="flex-center mb-8">
+          <GiCoffeeBeans className="w-15 h-15 text-mocha" />
+        </div>
+
+        <h1 className="card-title text-start mb-5  text-latte">Login</h1>
+
+        <label className="mb-1">Email:</label>
+        <div className="flex-center mb-8 border rounded-md border-latte px-3 focus-border-1 focus-within:border-espresso focus:outline-none">
+          <CiMail className="h-5 w-5 text-mocha" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border-none outline-none"
+          />
+        </div>
+
+        <label className="mb-1">Password:</label>
+        <div className="flex-center mb-8 border rounded-md border-latte px-3 focus-border-1 focus-within:border-espresso focus:outline-none">
+          <CiLock className="h-5 w-5 text-mocha" />
+          <input
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border-none outline-none"
+          />
+          {showPassword ? (
+            <LuEye
+              onClick={(e) => {
+                e.stopPropagation();
+                setShowPassword(!showPassword);
+              }}
+              className="h-5 w-5 text-latte cursor-pointer"
+            />
+          ) : (
+            <LuEyeClosed
+              onClick={(e) => {
+                e.stopPropagation();
+                setShowPassword(!showPassword);
+              }}
+              className="h-5 w-5 text-latte cursor-pointer"
+            />
+          )}
+        </div>
+
+        <div className="mb-8 flex-center justify-between">
+          <div className="flex-center gap-1 cursor-pointer">
+            <input type="checkbox" id="remember" className="accent-mocha" />
+            <label
+              htmlFor="remember"
+              className="text-sm text-espresso font-medium"
+            >
+              Remember me
+            </label>
+          </div>
+          <div className="flex-center gap-1 cursor-pointer">
+            <span className="text-sm text-espresso font-semibold">
+              Forgot Password?
+            </span>
+          </div>
+        </div>
+        <button className="primary-btn">Login</button>
+      </div>
+    </div>
   );
 }
--- frontend/src/shared/GuestNav/GuestHeader.jsx
+++ frontend/src/shared/GuestNav/GuestHeader.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
 export default function GuestHeader() {
   return (
-    <div>
-      <p>Guest Navbar</p>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-    </div>
+    <nav className="bg-espresso">
+      <p className="text-white">Guest Navbar</p>
+      <div className="">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/login" className="nav-link">
+          Login
+        </Link>
+        <Link to="/register" className="nav-link">
+          Register
+        </Link>
+      </div>
+    </nav>
   );
 }
